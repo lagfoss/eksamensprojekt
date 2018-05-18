35cb61fe771c7e698901bbb940bfd62bb44e70b1
Tca91 <tca47@live.dk>
2018-05-18 11:16:54 +0200

    php

--- contactform.php
+++ contactform.php
@@ -6,6 +6,15 @@ if (isset($_POST['submit'])){
   $subject = $_POST['subject'];
   $mailFrom = $_POST['mail'];
   $message = $_POST['message'];
+
+
+  $mailTo = "tca47@live.dk";
+  $header = "From: ".$mailFrom
+  $txt = "Tilmeding som frivillig fra ".$name.".\n\n".$message;
+
+  mail($mail_To, $subject, $txt, $header );
+  header("Location: index.php?mailsend");
+
 }
 
 
